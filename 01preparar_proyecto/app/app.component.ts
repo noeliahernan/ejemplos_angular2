0e7e4869f47f03f1136e951130a1fdcc281230fb
Noelia Hernan <noeliahernan1989@gmail.com>
2017-05-31 18:27:26 +0200

    Example 1 Angular 2
    Example 1 Angular 2

---
+++ 01preparar_proyecto/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h2>¡¡¡Hola Mundo con Angular 2!!!</h2>'
+})
+
+export class AppComponent {
+
+}
---
+++ 02template_multilinea/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h2>¡¡¡Hola Mundo con Angular 2!!!</h2>
+    <p>Nuevo app con template multilinea</p>
+    `
+})
+
+export class AppComponent {
+
+}
